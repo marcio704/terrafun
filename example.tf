5eb40c36cfbdbebc2226cf96acad283fa8b61658
Marcio Santiago <marcio704@yahoo.com.br>
2020-08-06 22:04:40 -0300

    First attemp

---
+++ example.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  profile = "studies"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example.id
+}
