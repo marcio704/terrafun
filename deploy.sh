144f28c150d1fccd5c69808bf4b44a48600e94eb
Kisters Ventura <kistters.code@gmail.com>
2020-08-12 23:56:23 -0300

    adding dockerbuild

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+docker build . -t terra-python:latest
+
 $(aws ecr get-login --region 'us-west-2' --no-include-email)
 
 docker tag $(docker images terra-python:latest -q) 455482506963.dkr.ecr.us-west-2.amazonaws.com/terra_images
