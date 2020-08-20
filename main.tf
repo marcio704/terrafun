dc7cfda5a42e0c072ccff4c8d73461661f1eaa85
Kisters Ventura <kistters.code@gmail.com>
2020-08-20 13:02:32 -0300

    remove hiffen

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ provider "aws" {
 }
 
 resource "aws_key_pair" "key_pair" {
-  key_name   = "key-pair--${var.environment}"
+  key_name   = "key-pair-${var.environment}"
   public_key = file("./terraform.pem")
 }
